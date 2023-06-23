633bb34915088802fe9d6c0a9f42cc166bd8cb59
NotoriousJat <nishantjat.dx@gmail.com>
2023-06-23 18:31:32 +0530

    v26

--- src/components/Leftsidebar/Leftsidebar.js
+++ src/components/Leftsidebar/Leftsidebar.js
@@ -239,10 +239,10 @@ function Leftsidebar() {
       <nav>
         <div>
           <button className={styles.openbtn} onClick={() => openSideNavbar()}>
-            <svg viewBox="0 0 100 70" width="30" height="18">
-              <rect width="100" height="7"></rect>
-              <rect y="30" width="100" height="7"></rect>
-              <rect y="60" width="100" height="7"></rect>
+            <svg viewBox="0 0 80 70" width="30" height="40">
+              <rect width="80" height="7"></rect>
+              <rect y="35" width="80" height="7"></rect>
+              <rect y="70" width="80" height="7"></rect>
             </svg>
           </button>
         </div>
