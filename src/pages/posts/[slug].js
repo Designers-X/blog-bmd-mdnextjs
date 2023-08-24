9ff075bbd44c8612e6ea06c531cfe30654819e93
Nishant jat <88760401+NotoriousJat@users.noreply.github.com>
2023-08-24 17:40:06 +0530

    24.07

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -216,7 +216,7 @@ export default function Post({ post, socialImage, related }) {
         />
         {/* </Container> */}
       </Content>
-      {relatedPosts.length > 0 && <RelatedPost array={getRandom(relatedPosts, 40)} />}
+      {relatedPosts.length > 0 && <RelatedPost array={getRandom(relatedPosts, 3)} />}
 
       {false && (
         <Section className={styles.postFooter}>
