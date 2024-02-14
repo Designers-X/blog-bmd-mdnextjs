50454081784725be03984f9081d46c976054943a
vinodchris <vinod09.dx@gmail.com>
2024-02-14 18:51:13 +0530

    vinod test page with error fixed add paragraph v1.5

--- src/pages/vinodTest.js
+++ src/pages/vinodTest.js
@@ -4,9 +4,19 @@ const Cart = () => {
   return (
     <Layout>
       <h1>Welcome Vinod To Test Page</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
+        standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book
       </p>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book </p>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
+        standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book{' '}
+      </p>
+
+      <p>This is third paragraph Three</p>
+      <p>This is third paragraph Fourth</p>
     </Layout>
   );
 };
